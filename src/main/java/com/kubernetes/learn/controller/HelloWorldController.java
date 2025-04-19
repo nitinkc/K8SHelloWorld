4c401c4db2af541e33ede63af3267d6298927b01
Nitin Chaurasia <gs.nitin@gmail.com>
2025-04-19 15:22:49 -0500

    updates

--- src/main/java/com/kubernetes/learn/controller/HelloWorldController.java
+++ src/main/java/com/kubernetes/learn/controller/HelloWorldController.java
@@ -14,28 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    private static final String DEFAULT_ENV_INSTANCE_GUID = "HOSTNAME" ;
-    private static final String HOST_NAME = "LOCAL";
+
 
     @Autowired
     Environment env;
     @Autowired
     InstanceInfoService iis;
 
-    // @Value(${ENVIRONMENT_VARIABLE_NAME:DEFAULT_VALUE})
-    @Value("${" + HOST_NAME + ":" + DEFAULT_ENV_INSTANCE_GUID + "}")
-    private String hostName;
-
     @GetMapping(path = "/")
     public String imUpAndRunning() {
         String port = env.getProperty("local.server.port");
-        String heath_check = "The Server is up and running on port : " + port + " with HostName : " + hostName;
+        String heath_check = "The Server is up and running on port : " + port;
 
-        return heath_check + iis.getFullHostName();
+        return heath_check +"\n"+ iis.getFullHostName() +"\n"+ iis.getAppInfo() +"\n" + iis.getLocalIpAddress();
     }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
-        return "Hi " ;
+        return "Hi " + iis.getAppInfo();
     }
 }
--- src/main/java/com/kubernetes/learn/services/InstanceInfoService.java
+++ src/main/java/com/kubernetes/learn/services/InstanceInfoService.java
@@ -4,6 +4,9 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Created by nichaurasia on Thursday, May/28/2020 at 10:49 AM
  */
@@ -18,11 +21,17 @@ public class InstanceInfoService {
     @Value("${default.env.instance.guid:defaultInstanceGUID}")
     private String instanceGuid;
 
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
     private String fullHostName;
 
     @PostConstruct
     public void init() {
-        this.fullHostName = hostName + " \n instance ID : " + instanceGuid;
+        this.fullHostName = hostName + " :: \n instance ID : " + instanceGuid;
     }
 
     public String getFullHostName() {
@@ -31,5 +40,18 @@ public class InstanceInfoService {
 
     public String retrieveInstanceInfo() {
         return hostName.substring(hostName.length()-5);
+    }
+
+    public String getAppInfo() {
+        return "App: " + appName + ", Version: " + appVersion;
+    }
+
+    public String getLocalIpAddress() {
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            return localHost.getHostAddress();  // e.g., 192.168.1.5
+        } catch (UnknownHostException e) {
+            return "Unknown";
+        }
     }
 }
